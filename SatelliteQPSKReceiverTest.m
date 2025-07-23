8c8e8de3a2fa7873417c74d3ea0889bdcc79bd6f
deeperthought <deeperthought@noreply.gitcode.com>
2025-07-23 10:54:14 +0800

    feat(course): 更新卫星通信课程期末项目并提供测试数据
    - 新增遥感卫星下行图像接收和解调课题
    - 提供航天宏图遥感卫星的下行数据用于接收端搭建和解调
    - 更新 SatelliteQPSKReceiverTest.m 中的参数配置
      - 采样率从 500e6 下调到 50e6
      - 重采样分母从 10 修改为 2
      - 重采样后的采样率和数传速率均调整为 75e6

--- SatelliteQPSKReceiverTest.m
+++ SatelliteQPSKReceiverTest.m
@@ -2,11 +2,11 @@ clc;clear;
 
 %% 定义参数对象
 config.inputDataFilename = "sample_06111";%611采集数据，约60G
-config.sourceSampleRate = 500e6;% 原始信号采样率
+config.sourceSampleRate = 50e6;% 原始信号采样率
 config.resampleMolecule = 3;% 重采样分子
-config.resampleDenominator = 10;% 重采样分母
-config.fs = 150e6;% 重采样后的采样率
-config.fb = 150e6;% 数传速率150Mbps
+config.resampleDenominator = 2;% 重采样分母
+config.fs = 75e6;% 重采样后的采样率
+config.fb = 75e6;% 数传速率75Mbps
 config.startBits = 200e6;% 文件读取数据的起始点
 config.bitsLength = 2e5;% 单次处理点
 config.IBytesFilename = "Ibytes.txt";% I路比特输出文件
